92dd2be6ba8936c282431ab2d8eec1057d359203
smahaduzzaman <smahaduzzaman96@gmail.com>
2023-06-04 02:28:21 +0600

    Sixth commit for eslint, prettier, husky and lint stage setup

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,11 @@ const app: Application = express()
 app.use(cors())
 app.use(express.json())
 
-app.use(express.urlencoded({ extended: true }))
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Working Successfully')
