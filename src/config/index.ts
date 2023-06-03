a3e7d9b0e5185bd24db55800b3b417777cb66451
smahaduzzaman <smahaduzzaman96@gmail.com>
2023-06-04 01:47:47 +0600

    For commit for setting eslint, prettier

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,22 +1,20 @@
-import dotenv from 'dotenv';
-import path from 'path';
+import dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
-    port: process.env.PORT || 8000,
-    database_url: process.env.DATABASE_URL,
+  port: process.env.PORT || 8000,
+  database_url: process.env.DATABASE_URL,
 
-    // db: {
-    //     host: process.env.DB_HOST || 'localhost',
-    //     port: process.env.DB_PORT || 27017,
-    //     name: process.env.DB_NAME || 'test',
-    //     user: process.env.DB_USER || '',
-    //     password: process.env.DB_PASSWORD || '',
-    // }
-
+  // db: {
+  //     host: process.env.DB_HOST || 'localhost',
+  //     port: process.env.DB_PORT || 27017,
+  //     name: process.env.DB_NAME || 'test',
+  //     user: process.env.DB_USER || '',
+  //     password: process.env.DB_PASSWORD || '',
+  // }
 }
-
 
 // IhruFS4RipvQHOPE
 // universityAdmin
